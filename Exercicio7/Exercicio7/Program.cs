dcb18af2f70323307e8f3d9d657b2f128ea3c77d
Paulo <107899591+pauloosmt@users.noreply.github.com>
2025-02-19 16:34:42 -0300

    Add files via upload

---
+++ Exercicio7/Exercicio7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercicio7 
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Enter the number: ");
+            x = int.Parse(Console.ReadLine()!);
+
+            if (x < 0)
+            {
+                Console.WriteLine("Negative");
+            }
+
+            else
+            {
+                Console.WriteLine("Positive");
+            }
+
+        }
+    }
+
+}
