c1906f08970d5ab68f494d2271f61ec01b4b145e
Paulo <107899591+pauloosmt@users.noreply.github.com>
2025-02-19 16:43:34 -0300

    Add files via upload

---
+++ Exercicio13/Exercicio13/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.WriteLine("Enter the coordinate 'x' and 'y' respectively: ");
+            string[] value = Console.ReadLine()!.Split(' ');
+            x = double.Parse(value[0], CultureInfo.InvariantCulture);
+            y = double.Parse(value[1], CultureInfo.InvariantCulture);
+
+            if (x == 0 && y == 0)
+            {
+                Console.WriteLine("Origin");
+            }
+            else if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Q4");
+            }
+            else if (x == 0)
+            {
+                Console.WriteLine("Axle Y");
+            }
+            else if (y == 0)
+            {
+                Console.WriteLine("Axle X");
+            }
+        }
+    }
+}
