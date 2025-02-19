518067512ac1d59c53750fe79127b7d86fb17548
Paulo <107899591+pauloosmt@users.noreply.github.com>
2025-02-19 16:40:01 -0300

    Add files via upload

---
+++ Exercicio8/Exercicio8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Enter the number: ");
+            x = int.Parse(Console.ReadLine()!);
+
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Par");
+            }
+            
+            else
+            {
+                Console.WriteLine("Ímpar");
+            }
+
+        }
+    }
+}
