995787ba445017e33036fb067373a422ce892e57
Paulo <107899591+pauloosmt@users.noreply.github.com>
2025-02-21 18:07:57 -0300

    Add files via upload

---
+++ Exercicio009/Exercicio9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("Enter the number: ");
+            a = int.Parse(Console.ReadLine()!);
+            Console.WriteLine("Enter other number: ");
+            b = int.Parse(Console.ReadLine()!);
+
+            if (a % b == 0 || b % a == 0)
+            {
+                Console.WriteLine("are multiple");
+            }
+            else
+            {
+                Console.WriteLine("they aren't multiple");
+            }
+        }
+    }
+}
+
