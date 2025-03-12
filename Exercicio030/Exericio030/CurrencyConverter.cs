2e0b6a29daa5c5deb92b8d71f554a2d844690491
Paulo <107899591+pauloosmt@users.noreply.github.com>
2025-03-11 23:30:05 -0300

    Add files via upload

---
+++ Exercicio030/Exericio030/CurrencyConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exericio030
+{
+    class CurrencyConverter
+    {
+        public static double Dollar;
+        
+        public static double Converter(double quant)
+        {
+            return Dollar * quant * 1.06;
+
+        }
+    }
+}
---
+++ Exercicio030/Exericio030/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Exericio030;
+
+namespace Exercicio030
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("What is the dollar exchange rate?: ");
+            CurrencyConverter.Dollar = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+            Console.Write("How many dollars will you buy?: ");
+            double quant = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Amount to be paid: R${CurrencyConverter.Converter(quant).ToString("F2", CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
