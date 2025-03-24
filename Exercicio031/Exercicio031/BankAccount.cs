4c8b41e7ee1088378a850225f5206008bc9e4328
Paulo <107899591+pauloosmt@users.noreply.github.com>
2025-03-24 11:26:35 -0300

    Add files via upload

---
+++ Exercicio031/Exercicio031/BankAccount.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+
+namespace Exercicio031
+{
+    class BankAccount
+    {
+        public int Number { get; private set; }
+        private string _name;
+        public double Balance { get; private set; }
+
+        public BankAccount( int number,string name)
+        {
+            Number = number;
+            _name = name;
+            
+        }
+
+        public BankAccount (int number, string name, double deposit) : this(number, name)
+        {
+            Deposit(deposit);
+        }
+
+        public void SetNumber(int number)
+        {
+            Number = number;
+        }
+
+       public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value != null)
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public void Deposit(double value)
+        {
+            Balance += value;
+        }
+
+        public void WithDraw(double value)
+        {
+            Balance -= value + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return ($"Account: {Number}, Holder: {_name}, Balance: R${Balance.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+
+        
+
+    }
+}
---
+++ Exercicio031/Exercicio031/Program.cs
@@ -0,0 +1,72 @@
+﻿/* Projeto de uma simulação de conta bancaria, onde o usuario informa: o numero da conta, o nome da conta,
+ e se deseja ou não realizar um saque inicial. Foi utlizado uma classe para criar o objeto da conta e também métodos simples
+de encapsulamento.
+
+Bank account simulation project, where the user informs: the account number, the account name, 
+and whether or not he/she wishes to make an initial withdrawal. A class was used to create the account object and also simple 
+encapsulation methods. */
+
+
+using System.Globalization;
+
+namespace Exercicio031
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount user;
+
+            Console.WriteLine("========================");
+            Console.WriteLine("NATIONAL BANK");
+            Console.WriteLine("========================");
+            Console.WriteLine();
+            Console.Write("Enter  the account number: ");
+            int number = int.Parse(Console.ReadLine()!);
+
+
+            Console.Write("Enter the account holder: ");
+            string name = Console.ReadLine()!;
+
+            Console.Write("Do you want to make an initial deposit (s/n)? ");
+            char resp = char.Parse(Console.ReadLine()!);
+
+            if (resp == 's' || resp == 'S')
+            {
+
+                Console.Write("What is the initial deposit? R$");
+                double balance = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+                user = new BankAccount(number, name, balance);
+
+            }
+            else
+            {
+                user = new BankAccount(number, name);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Account data: ");
+            Console.WriteLine(user);
+
+            Console.WriteLine();
+            Console.Write("Enter an amount to deposit: R$");
+            double dep = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+            user.Deposit(dep);
+
+            Console.WriteLine();
+            Console.WriteLine("Updated account data: ");
+            Console.WriteLine(user);
+
+            Console.WriteLine();
+            Console.Write("Enter an amount to withdraw: R$");
+            double wd = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+            user.WithDraw(wd);
+
+            Console.WriteLine();
+            Console.WriteLine("Updated account data: ");
+            Console.WriteLine(user);
+            
+        }
+
+    }
+}
