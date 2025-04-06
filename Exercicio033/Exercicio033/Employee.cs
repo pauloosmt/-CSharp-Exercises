14d50176f063f631413342a42cbf9ed5fab61661
Paulo <107899591+pauloosmt@users.noreply.github.com>
2025-04-06 18:26:40 -0300

    Add files via upload

---
+++ Exercicio033/Exercicio033/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Exercicio033
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public void IncreaseSalary(double percentage)
+        {
+            Salary += (Salary * percentage) / 100;
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"{Id}: {Name}, {Salary.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Exercicio033/Exercicio033/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Security.Cryptography;
+
+namespace Exercicio033
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("How many employees will be registered?: ");
+            n = int.Parse(Console.ReadLine()!);
+
+            List<Employee> user = new List<Employee>(); // lista criando objetos que virao a ser funcionarios
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i}:");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine()!);
+                Console.Write("Name: ");
+                string name = Console.ReadLine()!;
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine()!);
+                Console.WriteLine();
+
+                user.Add(new Employee { Id = id, Name = name, Salary = salary }); //instanciando a lista para criar novos usuarios e adiciona-los
+
+            }
+
+            Console.Write("Enter the employee id that will have salary increase : ");
+            int id_incr = int.Parse(Console.ReadLine()!); 
+            
+
+            Employee emp = user.Find(x => x.Id == id_incr)!; //conferindo se o id informado tem algum funcionário vinculado
+
+            if(emp != null) //se o emp encontrar um id diferente de nulo
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+                emp.IncreaseSalary(percentage);
+            } 
+            else
+            {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:");
+            foreach(Employee obj in user)
+            {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
